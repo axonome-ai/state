81241a9c1b02ae893d22f0b70493b6fd322fd828
Anton <anton.safarevich@gmail.com>
2025-08-26 11:12:06 -0700

    WIP on replace_ctrl

--- src/state/_cli/_tx/_preprocess_infer.py
+++ src/state/_cli/_tx/_preprocess_infer.py
@@ -98,6 +98,7 @@ def run_tx_preprocess_infer(
     logger.info("This creates a 'control template' where state_transition inference will apply perturbation effects")
     
     total_replaced_cells = 0
+    rng = np.random.default_rng(seed=seed)
     
     # For each non-control perturbation, replace with randomly sampled control cells
     for i, perturbation in enumerate(non_control_perturbations):
@@ -108,7 +109,7 @@ def run_tx_preprocess_infer(
         
         if n_pert_cells > 0:
             # Sample n_pert_cells control cells randomly (with replacement)
-            sampled_control_indices = np.random.choice(control_indices, size=n_pert_cells, replace=True)
+            sampled_control_indices = rng.choice(control_indices, size=n_pert_cells, replace=True)
             
             # Replace the expression data
             adata_modified.X[pert_indices] = adata.X[sampled_control_indices]

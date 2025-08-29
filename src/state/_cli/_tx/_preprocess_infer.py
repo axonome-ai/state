b30ab6f5e841a1c6159f197a213db4ca31959986
Anton <anton.safarevich@gmail.com>
2025-08-29 13:11:40 -0700

    undoing part

--- src/state/_cli/_tx/_preprocess_infer.py
+++ src/state/_cli/_tx/_preprocess_infer.py
@@ -84,12 +84,11 @@ def run_tx_preprocess_infer(
     adata_modified = adata.copy()
     
     # Get all unique perturbations (non-control)
-    # if hasattr(adata.obs[pert_col], 'cat'):
-    #     # Categorical column
-    #     unique_perturbations = adata.obs[pert_col].cat.categories
-    # else:
-        # Regular column
-    unique_perturbations = adata.obs[pert_col].unique()
+    if hasattr(adata.obs[pert_col], 'cat'):
+        # Categorical column
+        unique_perturbations = adata.obs[pert_col].cat.categories
+    else:
+        unique_perturbations = adata.obs[pert_col].unique()
     
     non_control_perturbations = [p for p in unique_perturbations if p != control_condition]
     

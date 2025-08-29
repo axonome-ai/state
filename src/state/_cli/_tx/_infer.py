c9a3c436aa52c0495d25ec4218235e691bd731ad
Anton <anton.safarevich@gmail.com>
2025-08-28 17:43:18 -0700

    adding logging

--- src/state/_cli/_tx/_infer.py
+++ src/state/_cli/_tx/_infer.py
@@ -229,14 +229,17 @@ def run_tx_infer(args):
 
             # Run inference on batch using padded=False like in working code
             batch_preds = model.predict_step(batch, batch_idx=batch_idx, padded=False)
-            # print(list(batch.keys()))
-            # print('batch["pert_emb"].shape', batch["pert_emb"].shape)
-            # print('batch["ctrl_cell_emb"].shape', batch["ctrl_cell_emb"].shape)
-            # print('cell_sentence_len', cell_sentence_len)
-            # print('batch["pert_name"]', batch["pert_name"])
-            # print('batch["pert_emb"]', batch["pert_emb"])
-            # print('batch["ctrl_cell_emb"]', batch["ctrl_cell_emb"])
-            # print('batch["preds"]',  batch_preds['preds'])
+            if batch_idx == 39:
+                print(list(batch.keys()))
+                print('batch["pert_emb"].shape', batch["pert_emb"].shape)
+                print('batch["ctrl_cell_emb"].shape', batch["ctrl_cell_emb"].shape)
+                print('cell_sentence_len', cell_sentence_len)
+                print('batch["pert_name"]', batch["pert_name"])
+                print('batch["pert_emb"]', batch["pert_emb"])
+                print('batch["ctrl_cell_emb"]', batch["ctrl_cell_emb"])
+                print('batch["preds"]',  batch_preds['preds'])
+                print()
+
 
 
             # Extract predictions from the dictionary returned by predict_step
